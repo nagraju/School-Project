06692e882ab905454a8875411d25eb34f528cf5e
grimen <grimen@gmail.com>
2009-11-26 04:11:37 +0100

    Fixing submodule conflict.

--- vendor/plugins/slicehost/lib/capistrano/ext/slicehost/sqlite.rb
+++
@@ -1,7 +0,0 @@
-namespace :sqlite do
-  desc "Install Sqlite3"
-  task :install, :roles => :db do
-    sudo "aptitude install -y sqlite3 libsqlite3-dev"
-    sudo "/usr/bin/gem install sqlite3-ruby"
-  end
-end
--- vendor/plugins/slicehost/lib/capistrano/ext/slicehost/utils.rb
+++
@@ -1,6 +0,0 @@
-namespace :utils do
-  desc "setup common system tools"
-  task :setup, :roles => :files do
-    sudo "aptitude install -y locate psmisc"
-  end
-end
